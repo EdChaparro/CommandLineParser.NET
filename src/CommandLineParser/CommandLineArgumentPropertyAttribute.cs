2eff83943ca674b39c81ae816ab618392ee91ca9
Ed Chaparro <ed@IntrepidProducts.com>
2020-09-04 11:20:45 -0400

    Add Command Line Argument Property Attribute

---
+++ src/CommandLineParser/CommandLineArgumentPropertyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntrepidProducts.CommandLineParser
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CommandLineArgumentPropertyAttribute : Attribute
+    {
+        public CommandLineArgumentPropertyAttribute
+            (String key, object defaultValue, string description, bool isUndocumented = false)
+        {
+            Key = key;
+            DefaultValue = defaultValue;
+            Description = description;
+            IsUndocumented = isUndocumented;
+        }
+
+        public string Key { get; }
+        public string Description { get; }
+        public object DefaultValue { get; }
+        public bool IsUndocumented { get; }
+    }
+}
