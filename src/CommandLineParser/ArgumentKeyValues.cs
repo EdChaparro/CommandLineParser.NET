8accc5a56d3a73a1d441760c2db43ec434fbf3f8
Ed Chaparro <ed@IntrepidProducts.com>
2020-09-04 01:25:23 -0400

    Add Argument Key-Value Parser and Tests

--- src/CommandLineParser/ArgumentKeyValues.cs
+++ src/CommandLineParser/ArgumentKeyValues.cs
@@ -1,10 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CommandLineParser
 {
     public class ArgumentKeyValues
     {
+        public ArgumentKeyValues(IEnumerable<string> args)
+        {
+            Parse(args);
+        }
+
+        private readonly Dictionary<string, string> _args =
+            new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+
+        private void Parse(IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                var kvArray = arg.Split('=');
+
+                if (kvArray.Length != 2)
+                {
+                    throw new ArgumentException($"Unable to parse into key-value, arg: {arg}");
+                }
+
+                var kvTuple = Clean(kvArray);
+
+                _args.Add(kvTuple.key, kvTuple.value);
+            }
+        }
+
+        private (string key, string value) Clean(string[] kvArray)
+        {
+            var key = CleanKey(kvArray[0]);
+            var value = CleanValue(kvArray[1]);
+
+            return (key, value);
+        }
+
+        private string CleanKey(string key)
+        {
+            if (key.StartsWith("--") && (key.Length > 2))
+            {
+                return key.Substring(2);
+            }
+
+            throw new ArgumentException($"Key must begin with -- and contain at least one char, arg: {key}");
+        }
+
+        private string CleanValue(string value)
+        {
+            if (value.StartsWith("\"") && value.EndsWith("\""))
+            {
+                return value.Trim('"');
+            }
+
+            if (value.StartsWith("'") && value.EndsWith("'"))
+            {
+                return value.Trim('\'');
+            }
+
+            if (value.StartsWith("\"") || value.EndsWith("\"") ||
+                value.StartsWith("'") || value.EndsWith("'"))
+            {
+                    throw new ArgumentException($"Missing quote, arg: {value}");
+            }
+
+            return value;
+        }
+
+        public string this[string key] => (_args.ContainsKey(key)) ? _args[key] : null;
     }
 }
--- src/Tests/Test.CommandLineParser/ArgumentKeyValuesTest.cs
+++ src/Tests/Test.CommandLineParser/ArgumentKeyValuesTest.cs
@@ -1,3 +1,5 @@
+using System;
+using CommandLineParser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test.CommandLineParser
@@ -6,8 +8,80 @@ namespace Test.CommandLineParser
     public class ArgumentKeyValuesTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldValidateArgumentsStartWithDoubleDash()
         {
+            var argKv = new ArgumentKeyValues(new string[] {"InvalidKey=SomeValue"});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldValidateArgumentHasMatchingDoubleQuotes()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--ValidKey=\"SomeValue" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldValidateArgumentHasMatchingSingleQuotes()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--ValidKey=SomeValue'" });
+        }
+
+        [TestMethod]
+        public void ShouldParseSingularArgument()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--Alpha=One" });
+            Assert.AreEqual("One", argKv["Alpha"]);
+        }
+
+        [TestMethod]
+        public void ShouldParseMultipleArguments()
+        {
+            var args = new string[]
+            {
+                "--key1=value1",
+                "--key2=value2"
+            };
+
+            var argKv = new ArgumentKeyValues(args);
+            Assert.AreEqual("value1", argKv["key1"]);
+            Assert.AreEqual("value2", argKv["key2"]);
+        }
+
+        [TestMethod]
+        public void ShouldNotIncludeValueQuotes()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--Key1=\"One\"" });
+            Assert.AreEqual("One", argKv["Key1"]);
+
+            argKv = new ArgumentKeyValues(new string[] { "--Key1='Two'" });
+            Assert.AreEqual("Two", argKv["Key1"]);
+        }
+
+        [TestMethod]
+        public void ShouldSupportValuesWithEmbeddedSpaces()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--Key1=\"One Two Three\"" });
+            Assert.AreEqual("One Two Three", argKv["Key1"]);
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreKeyCase()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--Key1=One" });
+            Assert.AreEqual("One", argKv["Key1"]);
+            Assert.AreEqual("One", argKv["KEY1"]);
+            Assert.AreEqual("One", argKv["key1"]);
+            Assert.AreEqual("One", argKv["KeY1"]);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenKeyIsUnknown()
+        {
+            var argKv = new ArgumentKeyValues(new string[] { "--Key1=One" });
+            Assert.AreEqual("One", argKv["Key1"]);
+            Assert.IsNull(argKv["Key2"]);
         }
     }
 }
